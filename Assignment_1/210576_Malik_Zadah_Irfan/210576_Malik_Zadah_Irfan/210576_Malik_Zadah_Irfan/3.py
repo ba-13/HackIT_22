f4536d931830427e5fe24e776b76cd96f8ec9ca3
malikzi21 <95172568+malikzi21@users.noreply.github.com>
2022-06-12 03:40:17 +0530

    Delete Assignment_1 directory

--- Assignment_1/210576_Malik_Zadah_Irfan/210576_Malik_Zadah_Irfan/210576_Malik_Zadah_Irfan/3.py
+++
@@ -1,9 +0,0 @@
-
-string = '8080'
-with open(r'C:\Users\Malik Zadah Irfan\Desktop\file1.txt', 'r') as fp:
-    
-    lines = fp.readlines()
-    for line in lines:    
-        if line.find(string) != -1:
-            print('Line Number:', lines.index(line))
-            print('Line:', line)
